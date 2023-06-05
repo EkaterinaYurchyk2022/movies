f03b01dbd5906459ac6f45db7e46eede8956488b
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2023-06-05 21:34:11 +0300

    feat: detail & video list

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -7,6 +7,9 @@ import apiConfig from '../../api/apiConfig';
 import './detail.scss';
 import Catalog from "../Catalog";
 import CastList from "./CastList";
+import VideoList from "./VideoList";
+
+import MovieList from '../../components/movie-list/MovieList';
 
 const Detail = () => {
 
@@ -57,6 +60,17 @@ const Detail = () => {
                                     </div>
                                    <CastList id={item.id}/>
                                 </div>
+                            </div>
+                        </div>
+                        <div className='container'>
+                            <div className='section mb-3'>
+                                <VideoList id={item.id}/>
+                            </div>
+                            <div className='section mb-3'>
+                                <div className='section__header mb-2'>
+                                    <h2>Similar</h2>
+                                </div>
+                                <MovieList category={category} type='similar' id={item.id}/>
                             </div>
                         </div>
                     </>
